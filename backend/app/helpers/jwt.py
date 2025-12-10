45fbbc958069e056c80f686a75630dfa2c11eafe
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2025-12-10 15:06:32 +0530

    add jwt function

---
+++ backend/app/helpers/jwt.py
@@ -0,0 +1,52 @@
+import jwt
+from datetime import datetime, timedelta
+from app.helpers.config import JWT_SECRET_KEY, JWT_ALGORITHM, JWT_TOKEN_TIME_HOURS
+from fastapi import Depends, HTTPException, status
+from app.helpers.database import get_db
+from fastapi.security import OAuth2PasswordBearer
+from app.models.usersModels import Users
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def create_jwt_token(data: dict):
+    to_encode = data.copy()
+    to_encode['exp'] = datetime.utcnow() + timedelta(hours=JWT_TOKEN_TIME_HOURS)
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        user_id: int = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(401, "Invalid token")
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired"
+        )
+
+    except jwt.InvalidSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token signature"
+        )
+
+    except jwt.DecodeError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Token is corrupted or malformed"
+        )
+
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+        )
+
+    user = db.query(Users).filter(Users.id == user_id).first()
+    return user
+
+
--- backend/app/routers/usersRoute.py
+++ backend/app/routers/usersRoute.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.schemas.usersSchema import UserCreateSchema, UserLoginSchema
 from app.models.usersModels import Users
 from app.helpers.hashing import hashPassword, verifyPassword
+from app.helpers.jwt import create_jwt_token, get_current_user
 
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -14,11 +15,11 @@ def registerUser(userRequest: UserCreateSchema, db: Session = Depends(get_db)):
     if userExist:
         raise HTTPException(status_code=400, detail="Email already registered")
     
-    hashPassword = hashPassword(userRequest.password)
+    hashPasswordData = hashPassword(userRequest.password)
 
     newUser = Users(
         email=userRequest.email,
-        password=hashPassword,
+        password=hashPasswordData,
         name=userRequest.name
     )
     db.add(newUser)
@@ -29,11 +30,17 @@ def registerUser(userRequest: UserCreateSchema, db: Session = Depends(get_db)):
 
 @router.post("/login")
 def loginUser(userRequest: UserLoginSchema, db: Session = Depends(get_db)):
-    dbUser = db.query(Users).flter(Users.email == userRequest.email).first()
-    if not dbUser:
+    db_user = db.query(Users).flter(Users.email == userRequest.email).first()
+    if not db_user:
         raise HTTPException(status_code=400, detail="Invalid email")
     
-    if not verifyPassword(userRequest.password, dbUser.password):
+    if not verifyPassword(userRequest.password, db_user.password):
         raise HTTPException(status_code=400, detail="Invalid password")
 
-    return {"message": "User logged in successfully"}
+    jwt_token = create_jwt_token({"user_id":db_user.id, "user_name": db_user.name, "user_email":db_user.email})
+
+    return {"message": "User logged in successfully", "token":jwt_token, "user_name": db_user.name, "user_email":db_user.email}
+
+@router.post("/profile")
+def get_user_profile(user: Users = Depends(get_current_user)):
+    return {"message": "User profile data", "user": {"id": user.id, "name": user.name, "email": user.email}}
